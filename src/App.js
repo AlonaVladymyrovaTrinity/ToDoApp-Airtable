17055e0cfd3d1973819b3175f862810552cd5017
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-01-18 03:17:28 -0800

    Assignment for Lesson-1-1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+const todoList = [
+  {
+    id: 1,
+    title: "Reed the book",
+  }, {
+    id: 2,
+    title: "Watch the videos",
+  }, {
+    id: 3,
+    title: "Complete assignment"
+  } ];
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+        <ul>
+          hint: {todoList.map (function (item) {
+          return <li key = {item.id}>
+                    <span>{item.id}</span>
+                    <span>{item.title}</span>
+                 </li>;
+                
+        })}
+        </ul>
     </div>
-  );
+    );
 }
 
 export default App;
