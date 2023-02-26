dd6f70ee82d7d9c46a0c7fbf0b16e8e1cd54b0fe
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-02-26 00:05:16 -0800

    InputWithLabel.js2

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -6,6 +6,9 @@ import { useRef } from "react";
 // so it runs first on mount and then on every re-render.The side-effect handler function
 // of the useEffect hook calls the focus() method on the current property of the inputRef
 // to set focus on the input element.
+
+// • You can store information between re-renders (unlike regular variables, which reset on every render).
+//  • Changing it does not trigger a re-render (unlike state variables, which trigger a re-render).
 let InputWithLabel = function (props) {
   const inputRef = useRef(null);
   React.useEffect(() => {
