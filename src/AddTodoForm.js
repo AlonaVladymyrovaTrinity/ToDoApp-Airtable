2241bfdbdb2fcb028e0c8864aaef4235fbfc70f5
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-02-26 19:18:08 -0800

    Lesson 1.3. another approach to solving the task added.

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,21 +1,52 @@
-import React from 'react'; 
+import React from "react";
 
-let AddTodoForm = function(props) {
-    const [inputText, setInputText] = React.useState("");
-    let handleAddTodo = function (event) {
-        event.preventDefault(); // prevent the default behavior of the form submit      
-        let todoTitle =  inputText; // retrieve the value of the title element from the event target and store it in a variable named todoTitle
-        console.log(todoTitle); //Log the value of todoTitle in the console
-        setInputText(''); //Reset the form so the text input value is cleared
-        props.onAddTodo(todoTitle);//  Inside the handleAddTodo function, invoke the onAddTodo callback prop and pass todoTitle as an argument
-      }
-return (
+let AddTodoForm = function (props) {
+  //-----Another approach----
+  // const handleAddTodo = (event) => {
+  //     event.preventDefault();
+  //     let todoTitle = event.target.title.value;
+  //     //ALL OF THESE OPTIONS ARE WORKING!
+  //     let todoTitle = event.target.title.value;
+  //     //let todoTitle = event.target.elements.title.value;
+  //     //let todoTitle = event.target.elements["todoTitle"].value;
+  //     //let todoTitle = event.target[0].value;
+  //     console.log(todoTitle);
+  //     props.onAddTodo(todoTitle);
+  //     //Clears out the text from input text box
+  //     //ALL OF THESE OPTIONS ARE WORKING!
+  //     //event.target.reset();
+  //     event.target.title.value = "";
+  // };
+
+  const [inputText, setInputText] = React.useState("");
+  let handleAddTodo = function (event) {
+    event.preventDefault(); // prevent the default behavior of the form submit
+    let todoTitle = inputText; // retrieve the value of the title element from the event target and store it in a variable named todoTitle
+    console.log(todoTitle); //Log the value of todoTitle in the console
+    setInputText(""); //Reset the form so the text input value is cleared
+    props.onAddTodo(todoTitle); //  Inside the handleAddTodo function, invoke the onAddTodo callback prop and pass todoTitle as an argument
+  };
+  //-----Another approach----
+  //   return (
+  //     <form onSubmit={handleAddTodo}>
+  //       <label htmlFor="todoTitle">Title:</label>
+  //       <input name="title" type="text" id="todoTitle" />
+  //       <button type="submit">Add</button>
+  //     </form>
+  //   );
+
+  return (
     <form onSubmit={handleAddTodo}>
-        <label htmlFor = "todoTitle">Title</label>
-        <input id = "todoTitle" name="title" value={inputText} onChange={(event) => setInputText(event.target.value)}></input>
-        <button type="submit">Add</button>
+      <label htmlFor="todoTitle">Title</label>
+      <input
+        id="todoTitle"
+        name="title"
+        value={inputText}
+        onChange={(event) => setInputText(event.target.value)}
+      ></input>
+      <button type="submit">Add</button>
     </form>
-)
-}
+  );
+};
 
 export default AddTodoForm;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+//  import React from 'react';
+//  import ReactDOM from 'react-dom';
+//  import App from './App';
 
-ReactDOM.render(
+//  ReactDOM.render(
+//    <React.StrictMode>
+//      <App />
+//    </React.StrictMode>,
+//    document.getElementById('root')
+//  );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
