74c900e1de8bfccfaf569a38cf938d6d85182bc6
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-02-27 02:16:59 -0800

    Assignment to lesson 1-7 | Asynchronous Data, Conditional Rendering, Advanced and Impossible State

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
-let useSemiPersistentState = () => {
-  const [todoList, setTodoList] = React.useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || []
-  );
-  React.useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
-  return [todoList, setTodoList];
-};
 
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState("");
-  // This code creates a new array called newTodoList by filtering the existing
-  // todoList array to remove the item with the given id. Then, it calls the
-  // setTodoList state setter and passes the new newTodoList array as its argument
-  // to update the state with the modified todoList.
+  const [todoList, setTodoList] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    new Promise((resolve, reject) =>
+      setTimeout(
+        () =>
+          resolve({
+            data: {
+              todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
+            },
+          }),
+        2000
+      )
+    ).then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }
+  }, [todoList, isLoading]);
+
   let removeTodo = function (id) {
     const newTodoList = todoList.filter((todo) => todo.id !== id); //The filter method creates a new array with all the elements that pass the conditions specified by a given function and returns the new array.
     setTodoList(newTodoList);
@@ -29,7 +41,11 @@ function App() {
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      <TodoList onRemoveTodo={removeTodo} todoList={todoList} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <TodoList onRemoveTodo={removeTodo} todoList={todoList} />
+      )}
     </>
   );
 }
